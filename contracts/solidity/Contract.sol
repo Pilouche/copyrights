b93ec4fc081b3af7a779afbeaf1af334dc50cf3a
pljanand <pljanand@10.43.6.250>
2018-10-23 09:44:38 +0200

    On continue le contrat

--- contracts/solidity/Contract.sol
+++ contracts/solidity/Contract.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.4.25;
         function kill() public { if (msg.sender == owner) selfdestruct(owner); }
 	}
 	
-	contract copyrights is mortal{
+	contract copyrights is mortal {
 		struct Investor {
 			address addr;
         	uint investedAmount;
@@ -26,27 +26,51 @@ pragma solidity ^0.4.25;
 		
 		struct Artwork {
 			string name;
-			string type;
+			string category;
 			Artist artist;
 			Producer producer;
         	uint worth;
         	uint numInvestors;
+        	uint numClients;
         	uint fare;
-        	mapping (uint => Investor) Investors;
-        	mapping (uint => Client) Clients;
+        	mapping (address => Investor) Investors;
+        	mapping (address => Client) Clients;
     	}
     	
     	uint numArtworks;
     	mapping (uint => Artwork) artworks;
     	
-    	function newArtwork(string name, string type, address artistAddr, address producerAddr, 
+    	function newArtwork(string name, string category, address artistAddr, address producerAddr, 
     		uint worth, uint fare) public returns (uint artworkID) {
         	artworkID = numArtworks++;
-        	artworks[artworkID] = Artwork(name, type, Artist(artistAddr), Producer(producerAddr), worth, 0, fare);
+        	artworks[artworkID] = Artwork(name, category, Artist(artistAddr), Producer(producerAddr, 0), worth, 0, 0, fare);
+    	}
+    	
+    	function productionInvestment(uint artworkID) public payable {
+    	    require(msg.value > 0);
+    		artworks[artworkID].artist.addr.transfer(msg.value);
+    		artworks[artworkID].producer.investedAmount += msg.value;
+    	}
+    	
+    	function privateInvestment(uint artworkID) public payable {
+    	    require(msg.value > 0);
+    		artworks[artworkID].artist.addr.transfer(msg.value);
+    		artworks[artworkID].numInvestors++;
+    		artworks[artworkID].Investors[msg.sender] = Investor(msg.sender, msg.value);
     	}
     	
     	function buyCopy(uint artworkID) public payable {
     		require(msg.value == artworks[artworkID].fare);
-    		
+    		artworks[artworkID].producer.addr.transfer(msg.value);
+    		artworks[artworkID].numClients++;
+    		artworks[artworkID].Clients[msg.sender] = Client(msg.sender);
+    	}
+    	
+    	function sellRights(uint artworkID, address producerAddr) public payable {
+    	    require(msg.value == artworks[artworkID].worth);
+    	}
+    	
+    	function payShares() {
+    	    
     	}
 	}
